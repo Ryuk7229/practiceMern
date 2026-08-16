ddf3dc2e66f9a3bab33443371a34ce1434340c36
Ryuk7229 <rohanporje7229@gmail.com>
2026-08-16 13:45:13 +0530

    ArrayFunctions

--- RohanPorje_Task15/RohanPorje_Task15/Task15/script.js
+++ RohanPorje_Task15/RohanPorje_Task15/Task15/script.js
@@ -7,11 +7,24 @@ let findMax = arr => Math.max(...arr);
 console.log("Maximum number:", findMax(arr))
 
 
+let findMin = arr => Math.min(...arr);
+console.log("Minimum number:", findMin(arr));
+
+
+
 // Anonymous function - calculate sum
 let calculateSum = function (arr) {
   return arr.reduce((sum, num) => sum + num, 0);
 };
 console.log("Sum of all elements:", calculateSum(arr));
+
+// Multipliction of numbers
+let calculateMul = function (arr) {
+  return arr.reduce((sum, num) => sum * num);
+};
+console.log("Multiplication of all elements:", calculateMul(arr));
+
+
 
 
 // Named function - count odd numbers
@@ -19,3 +32,15 @@ function countOdds(arr) {
   return arr.filter(num => num % 2 !== 0).length;
 }
 console.log("Count of odd numbers:", countOdds(arr));
+
+
+
+//  count even numbers
+
+function countEven(arr) {
+    return arr.filter(num => num %2 ==0).length;
+}
+console.log("Count of even number:", countEven(arr));
+
+
+
